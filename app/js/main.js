7c09c8b627ed687ca4a648f789469a640f8d3835
All4site <all4sitecomua@gmail.com>
2018-08-28 21:07:29 +0300

    work

--- app/js/main.js
+++ app/js/main.js
@@ -9,5 +9,42 @@ $(document).ready(function () {
 	});
 
 	$('.calendar').multiDatesPicker();
+//Accordeon
+	$('.faqAccordeon>.bottomAccordeon').not(':first').hide();
+
+	$('.faqAccordeon>.topAccordeon').click(function () {
+		var all = $('.faqAccordeon>.topAccordeon').removeClass('active');
+		var top = $(this).addClass('active');
+
+		var findAcc = $(this).next('.bottomAccordeon');
+		var findClosest = $(this).closest('.faqAccordeon');
+
+		if (findAcc.is(':visible')) {
+			findAcc.slideUp(1000);
+		} else {
+			findClosest.find('>.bottomAccordeon').slideUp(1000);
+			findAcc.slideDown(1000);
+		}
+
+	});
+
+// Accordeon courses
+	$('.coursesAccordeon>.bottomAccordeon').not(':first').hide();
+
+	$('.coursesAccordeon>.topAccordeon').click(function () {
+		var all = $('.coursesAccordeon>.topAccordeon').removeClass('active');
+		var top = $(this).addClass('active');
+
+		var findAcc = $(this).next('.bottomAccordeon');
+		var findClosest = $(this).closest('.coursesAccordeon');
+
+		if (findAcc.is(':visible')) {
+			findAcc.slideUp(1000);
+		} else {
+			findClosest.find('>.bottomAccordeon').slideUp(1000);
+			findAcc.slideDown(1000);
+		}
+
+	});
 
 });
